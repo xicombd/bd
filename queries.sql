be15a609217e74e716d07249719ce8bb20d58e8e
Raicuparta <raicuparta@gmail.com>
2014-12-10 20:03:17 +0000

    querries e triggers

---
+++ queries.sql
@@ -0,0 +1,74 @@
+//QUERIES
+
+//ex1
+
+SELECT C.pessoa
+FROM concorrente C
+WHERE C.pessoa NOT IN
+(
+  SELECT L.pessoa
+  FROM lance L
+);
+
+//ex2
+SELECT nome
+FROM pessoa, pessoac, concorrente
+WHERE pessoac.nif = concorrente.pessoa AND pessoac.nif = pessoa.nif
+GROUP BY pessoa.nome
+HAVING COUNT(*) = 2;
+
+//ex3
+SELECT dia, nif, nrleilaonodia
+FROM (
+  SELECT dia, nif, nrleilaonodia, MAX(ratio)
+  FROM (
+    SELECT leilao.dia, leilao.nif, leilao.nrleilaonodia, MAX(lance.valor/leilao.valorbase) as ratio
+    FROM leilao, lance, leilaor
+    WHERE lance.leilao = leilaor.lid
+    AND leilao.nif = leilaor.nif AND leilao.dia = leilaor.dia AND leilao.nrleilaonodia = leilaor.nrleilaonodia
+    GROUP BY leilaor.lid
+  ) as tabela1
+) as tabela2;
+
+
+//ex4
+SELECT DISTINCT pessoa.nome, p1.nif, p1.capitalsocial
+FROM pessoac as p1, pessoac as p2, pessoa
+WHERE p1.capitalsocial = p2.capitalsocial AND p1.nif != p2.nif AND pessoa.nif = p1.nif
+ORDER BY capitalsocial;
+
+
+
+//TRIGGERS
+
+//ex1
+delimiter //
+CREATE TRIGGER lance_base BEFORE INSERT ON lance
+FOR EACH ROW
+BEGIN
+  IF NEW.valor < (SELECT valorbase
+		  FROM leilao, leilaor
+		  WHERE NEW.leilao = leilaor.lid
+		    AND leilaor.nif = leilao.nif
+		    AND leilaor.dia = leilao.dia
+		    AND leilaor.nrleilaonodia = leilao.nrleilaonodia)
+  THEN
+    CALL lance_deve_ser_maior_que_base();
+  END IF;
+END //
+delimiter ;
+
+
+//ex2
+delimiter //
+CREATE TRIGGER lance_crescente BEFORE INSERT ON lance
+FOR EACH ROW
+BEGIN
+IF NEW.valor <= ANY(SELECT valor
+FROM lance
+WHERE 40 = lance.leilao)
+  THEN
+    CALL lance_deve_ser_crescente();
+  END IF;
+END //
+delimiter ;
--- registo.php
+++ registo.php
@@ -53,10 +53,11 @@
     echo "<p>Leiloes em curso ou a iniciar: </p>\n";
     
     // Apresenta os leilões
-    $sql = "SELECT *, DATEDIFF(DATE_ADD(leilaor.dia, INTERVAL leilaor.nrdias DAY), CURDATE()) as diasrestantes
-			FROM leilaor, leilao
-			WHERE leilaor.nif = leilao.nif AND leilaor.dia = leilao.dia AND leilaor.nrleilaonodia = leilao.nrleilaonodia
-			ORDER BY leilaor.lid ASC";
+    $sql = "SELECT *
+      FROM leilaor, leilao
+      WHERE leilaor.nif = leilao.nif AND leilaor.dia = leilao.dia AND leilaor.nrleilaonodia = leilao.nrleilaonodia
+      HAVING DATEDIFF(DATE_ADD(leilaor.dia, INTERVAL leilaor.nrdias DAY), CURDATE()) > 0
+      ORDER BY leilaor.lid ASC";
     $result = $connection->query($sql);
     if(!$result) {
   		echo("<p> Erro na Query:($sql) <p>");
@@ -65,20 +66,43 @@
     echo("<table border=\"1\">\n");
     echo("<tr><td>lid</td><td>nif</td><td>dia</td><td>NrDoDia</td><td>nome</td><td>tipo</td><td>valorbase</td><td>nrdias</td></tr>\n");
     foreach($result as $row){
-  		if ($row["diasrestantes"] > 0) {
-    		$data1 = strtotime($row["dia"]);
-    		$fim = $data1 + ($row["nrdias"] * 3600 * 24);
+  		//if ($row["diasrestantes"] > 0) {
 
     		echo("<tr><td>");
     		echo($row["lid"]); echo("</td><td>");
-			echo($row["nif"]); echo("</td><td>");
+		echo($row["nif"]); echo("</td><td>");
     		echo($row["dia"]); echo("</td><td>");
     		echo($row["nrleilaonodia"]); echo("</td><td>");
     		echo($row["nome"]); echo("</td><td>");
     		echo($row["tipo"]); echo("</td><td>");
     		echo($row["valorbase"]); echo("</td><td>");
     		echo($row["nrdias"]);
-      }
+     // }
+    }
+    echo("</table>\n");
+    
+    
+    // teste query 1
+    $sql = "SELECT C.pessoa
+	    FROM concorrente C
+	    WHERE C.pessoa NOT IN
+	    (
+	      SELECT L.pessoa
+	      FROM lance L
+	    )";
+    $result = $connection->query($sql);
+    if(!$result) {
+  		echo("<p> Erro na Query:($sql) <p>");
+  		exit();
+    }
+    
+    echo("<table border=\"1\">\n");
+    echo("<tr><td>leilao</td><td>pessoa</td></tr>\n");
+    foreach($result as $row){
+  		
+	echo("<tr><td>");
+	echo($row["pessoa"]);
+
     }
     echo("</table>\n");
     
